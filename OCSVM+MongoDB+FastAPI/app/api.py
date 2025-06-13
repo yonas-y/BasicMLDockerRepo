edc91eae80209e9adf3b315f630f0a5ec1b5a720
Yonas Y <yoni99y@gmail.com>
2025-06-14 01:07:55 +0300

    Added a retraining end point in the api!

--- OCSVM+MongoDB+FastAPI/app/api.py
+++ OCSVM+MongoDB+FastAPI/app/api.py
@@ -5,7 +5,9 @@ FastAPI implementation for the OCSVM network traffic anomaly detector!
 from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
 import numpy as np
-from model import load_model
+from app.model import load_model
+from app.train import train_ocsvm, evaluate_model
+from app.data_loader import load_data
 
 app = FastAPI(title="OCSVM Anomaly Detection API")
 
@@ -16,6 +18,10 @@ model, scaler = load_model()
 class InferenceInput(BaseModel):
     features: list
 
+@app.get("/")
+def root():
+    return {"message": "OCSVM Anomaly Detection API is running"}
+
 @app.post("/predict/")
 def predict(features: InferenceInput):
     try:
@@ -25,3 +31,22 @@ def predict(features: InferenceInput):
         return {"prediction": "normal" if pred == 1 else "anomaly"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/retrain/")
+def retrain(data_fraction: float= Query(1.0, ge=0.01, le=1.0)):
+    try:
+        df = load_data()
+        df_frac = df[:int(len(df) * data_fraction)]
+
+        # Evaluate the performance of the model on the training data!
+        df_normal = df_frac[df_frac["label"] == 2]
+
+        performance_metric = evaluate_model(df_normal, model, scaler)
+
+        return {
+            "message": "Model retrained successfully!",
+            "data_fraction": data_fraction,
+            "metrics": performance_metric
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
