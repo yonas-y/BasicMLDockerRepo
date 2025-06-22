a6513dfc83b5ca2ebf849a1c11306d5f96cab864
Yonas Y <yoni99y@gmail.com>
2025-06-22 21:55:22 +0300

    Updated the config file to set the IP address to connect MongoDB!

--- OCSVM+MongoDB+FastAPI/app/config.py
+++ OCSVM+MongoDB+FastAPI/app/config.py
@@ -3,7 +3,9 @@ Contains Configuration (DB URI, names, paths) for OCSVM+MongoDB+FastAPI.
 """
 
 # app/config.py
-MONGO_URI = "mongodb://host.docker.internal:27017"  # or "localhost:27017" if not in Docker
+MONGO_URI = "mongodb://192.168.80.1:27017"  # or "localhost:27017" if not in Docker
+uri = f"mongodb://{host_ip}:{port}"
+client = MongoClient(uri, serverSelectionTimeoutMS=3000)
 DB_NAME = "NSL-KDD"
 COLLECTION_NAME = "TrainData"
 MODEL_DIR = "output"
